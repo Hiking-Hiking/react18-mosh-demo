06a47422835696281f94407760a55e17ddfae508
小帅 <2502797161@qq.com>
2024-08-09 19:02:52 +0800

    新增了更新列表项的功能

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,10 @@ function App() {
     // fetchUsers();
     // // ------------------------------------------------------
   }, []);
+  /**
+   * 删除列表项
+   * @param user 点击要删除的列表项
+   */
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
@@ -65,12 +69,15 @@ function App() {
         setUsers(originalUsers);
       });
   };
+  /**
+   * 新增列表项
+   */
   const addUser = () => {
     const originalUsers = [...users];
     const newUser = { id: 0, name: "Mosh" };
     setUsers([newUser, ...users]);
     axios
-      .post("https://jsonplaceholder.typicode.com/usersd", newUser)
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
       .then(({ data: savedUser }) => {
         // console.log(res);
         setUsers([savedUser, ...users]);
@@ -80,7 +87,26 @@ function App() {
         setUsers(originalUsers);
       });
   };
-
+  /**
+   * 更新列表项
+   * @param user 当前点击的列表项
+   */
+  const updateUser = (user: User) => {
+    const originalUser = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    // http方法中，put一般是替换对象，patch一般是修补或者更新其一个或多个属性，实际操作中要根据后端是如何构建的，比如有些后端只支持put方法，不支持patch方法；在这个案例中，我们只需要更新一个属性，所以使用patch方法；
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .then((res) => console.log(res))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUser);
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -95,13 +121,20 @@ function App() {
             className="list-group-item d-flex justify-content-between"
           >
             {user.name}
-
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
